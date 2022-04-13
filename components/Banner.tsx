9f79896d730e872efdc0001b06f79813a7073eae
ittisafur <ittisafur@gmail.com>
2022-04-14 04:44:07 +0600

    Minor Changes

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -8,7 +8,7 @@ const cx = bindClassNames.bind(styles);
 const Banner = memo(() => {
   return (
     <div className={cx("banner")}>
-      <h1>Banana Club Boys</h1>
+      <h1>Banana Boys Club</h1>
       <button>Coming Soon</button>
     </div>
   );
