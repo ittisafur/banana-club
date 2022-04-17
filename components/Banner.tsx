f0cbe7b858d284b4d7ed93fc719d225cc3b4e05f
ittisafur <ittisafur@gmail.com>
2022-04-17 23:54:19 +0600

    Optimizing 3

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,4 +1,5 @@
 import { memo } from 'react';
+import Image from 'next/image';
 import bindClassNames from 'classnames/bind';
 
 import styles from '@/styles/banner.module.scss';
@@ -8,8 +9,19 @@ const cx = bindClassNames.bind(styles);
 const Banner = memo(() => {
     return (
         <div className={cx('banner')}>
-            <h1>Banana Boys Club</h1>
-            <button>Coming Soon</button>
+            <div className={cx('image')}>
+                <Image
+                    src="/assets/images/banner.jpg"
+                    layout="fill"
+                    objectFit="cover"
+                    objectPosition="center"
+                    alt="Banana Banner"
+                />
+            </div>
+            <div className={cx('content')}>
+                <h1>Banana Boys Club</h1>
+                <button>Coming Soon</button>
+            </div>
         </div>
     );
 });
